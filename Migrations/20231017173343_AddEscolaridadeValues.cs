49315f3e4a6898b8485611be91e90f2f27333ca6
Bruno Barreto <brunobarretosantos@gmail.com>
2023-10-17 14:35:56 -0300

    add escolaridade items by migration

---
+++ Migrations/20231017173343_AddEscolaridadeValues.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEscolaridadeValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Escolaridades",
+                columns: new[] { "Id", "Descricao" },
+                values: new object[,]
+                {
+                    { 1, "Infantil" },
+                    { 2, "Fundamental" },
+                    { 3, "Médio" },
+                    { 4, "Superior" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
